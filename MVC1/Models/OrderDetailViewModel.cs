25b7d52b3ed85fce6c7c45d135ac893973c4cd0a
Sourav Maji <maji.edu@gmail.com>
2019-11-12 10:00:00 +0530

    Updated MVC with pagination and comments(Return)

---
+++ MVC1/Models/OrderDetailViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using GreatOutdoors.Entities;
+
+namespace GreatOutdoors.Mvc.Models
+{
+    /*Order ViewModel
+    Project name : Great Outdoors
+    Developer name: Sourav Maji
+    Use case : Return
+    Creation date : 30/10/2019
+    Last modified : 08/11/2019
+    */
+    public class OrderDetailViewModel
+    {
+        //Creating the necessary properties for OrderDetails,PlaceReturn,RequestCancel view
+        public System.Guid OrderDetailID { get; set; }
+        public System.Guid OrderID { get; set; }
+        public System.Guid ProductID { get; set; }
+        public int Quantity { get; set; }
+        public decimal DiscountedUnitPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+        public System.Guid AddressID { get; set; }
+        public string CurrentStatus { get; set; }
+
+
+
+        public string ProductName { get; set; }
+
+
+
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PinCode { get; set; }
+        public string MobileNo { get; set; }
+
+        public string[] Reasons = { "Wrong", "Incomplete" };
+
+        [Required(ErrorMessage = "Reason of Return cannot be blank")]
+        public string ReasonOfReturn { get; set; }
+    }
+}
---
+++ MVC1/Models/OrderViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using GreatOutdoors.Entities;
+namespace GreatOutdoors.Mvc.Models
+{
+
+    /*Order ViewModel
+    Project name : Great Outdoors
+    Developer name: Sourav Maji
+    Use case : Return
+    Creation date : 30/10/2019
+    Last modified : 07/11/2019
+    */
+    public class OrderViewModel
+    {
+        //Creating the necessary properties for OrderHistory View
+        public System.Guid OrderID { get; set; }
+        public System.Guid RetailerID { get; set; }
+        public System.Guid SalespersonID { get; set; }
+        public string RetailerName { get; set; }
+        public int TotalQuantity { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string ChannelOfSale { get; set; }
+        public DateTime OrderDateTime { get; set; }
+        public DateTime ModifiedDateTime { get; set; }
+    }
+}
